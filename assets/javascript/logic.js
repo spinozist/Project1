4e7f69f386a74d98e8b56dce97cae3836fcc7d5b
Spinozist <ewoodworth@atlantaregional.org>
2018-09-27 21:28:33 -0400

    baby steps

---
+++ assets/javascript/logic.js
@@ -0,0 +1,12 @@
+window.onload = function() { 
+    // create web audio api context
+    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+// create Oscillator node
+var oscillator = audioCtx.createOscillator();
+
+oscillator.type = 'square';
+oscillator.frequency.setValueAtTime(200, audioCtx.currentTime); // value in hertz
+oscillator.connect(audioCtx.destination);
+oscillator.start();
+}
