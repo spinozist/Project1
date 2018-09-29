b9ccb33fc80adc4bc30558e4e9d4b087e2eef02e
Spinozist <ewoodworth@atlantaregional.org>
2018-09-29 15:24:10 -0400

    connection achieved

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,6 +1,52 @@
 window.onload = function () {
 
-    
+    $(`#user-you`).click(function () {
+
+
+        // Initialize Firebase
+        var config = {
+            apiKey: "AIzaSyDRol3DvQ2343emS0aQ-iJGHsaGuCGbECc",
+            authDomain: "noise-manipulator.firebaseapp.com",
+            databaseURL: "https://noise-manipulator.firebaseio.com",
+            projectId: "noise-manipulator",
+            storageBucket: "noise-manipulator.appspot.com",
+            messagingSenderId: "464135295056"
+        };
+
+        firebase.initializeApp(config);
+
+        var database = firebase.database();
+
+        var connectionLog = database.ref(`/connectedUsers`);
+
+        var connectedRef = database.ref(".info/connected");
+
+        // var remoteAudioParams = database.ref(`/audioContexts`);
+
+        // When the client's connection state changes...
+        connectedRef.on("value", function (snapshot) {
+
+            console.log(snapshot.key);
+            console.log(connectionLog);
+
+
+            // If they are connected..
+            if (snapshot.val()) {
+
+                // Add user to the connections list.
+                var connection = connectionLog.push(true);
+                console.log(snapshot.id);
+                console.log(snapshot);
+
+
+                // Remove user from the connection list when they disconnect.
+                connection.onDisconnect().remove();
+            }
+        });
+    })
+
+
+
     // create web audio api context
     var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
@@ -28,16 +74,16 @@ window.onload = function () {
 }
 
 // synthpad function
-var SynthPad = function() {
+var SynthPad = function () {
     myCanvas = document.getElementById('touchpad');
     frequencyLabel = document.getElementById('frequency');
     volumeLabel = document.getElementById('volume');
-  
+
     // Create an audio context.
     myAudioContext = new webkitAudioContext();
-  
+
     SynthPad.setupEventListeners();
-  };
-  
-  // Export SynthPad.
-  return SynthPad;
+};
+
+// Export SynthPad.
+// return SynthPad;
